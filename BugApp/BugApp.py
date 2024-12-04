29f45034b44d3d1e32d375d367ede5f749e97ebe
Str3l0k-Hack <wolfbottaillour@gmail.com>
2024-12-04 10:57:15 +0000

    still not finished v1
    added  documentation and visual better nice code looks pretty

--- BugApp/BugApp.py
+++ BugApp/BugApp.py
@@ -2,11 +2,12 @@ import os
 
 while (True):
 
+	print("\nWelcome to 'BugApp', An App for automating Recon\n")
 	print("Subfinder [1]")
 	print("Amass [2]")
 	print("Nmap [3]")
 	print("BurpSuite [4]")
-
+	print("\nDocumentation for all tools [5]")
 	choice = input(" = ")
 
 	if choice == '1':
@@ -16,4 +17,17 @@ while (True):
 	elif choice =='2':
 		print("\n")
 		os.system('python3 amass_script.py')
+
+	elif choice =='3':
+		print("\n")
+		os.system('python3 nmap_script.py')
+
+	elif choice =='4':
+		print("\n")
+		os.system('python3 burp_script.py')
+
+	elif choice =='5':
+		print("\n")
+		os.system('python3 doc_script.py')
+
 	break
